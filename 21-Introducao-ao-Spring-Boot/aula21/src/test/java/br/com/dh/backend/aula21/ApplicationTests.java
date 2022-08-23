043991cb0036a25fa5d07824b603290974a988fc
Fabiana Sato Kayanoki <89108256+FabianaYSK@users.noreply.github.com>
2022-08-23 10:36:52 -0300

    Aulas da Semana 4

---
+++ 21-Introducao-ao-Spring-Boot/aula21/src/test/java/br/com/dh/backend/aula21/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dh.backend.aula21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
